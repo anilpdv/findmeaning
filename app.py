8277da1f0af6a38f8c4379c8ccb1162272c74c3c
anilpdv <pdvanil007@gmail.com>
2019-04-13 06:37:19 +0530

    fixing vulnerability in jinja

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
-dictionary = {"definitions":[]}
+dictionary = {"definitions": []}
+
+
+# app route root path
 @app.route('/')
 def index():
     return "hello world"
 
+
+# app route search
+# params
 @app.route('/search/<string:query>')
 def search(query):
     url = 'https://www.vocabulary.com/dictionary/' + query
     res = requests.get(url)
-    html = BeautifulSoup(res.text,features="html.parser")
-    
+    html = BeautifulSoup(res.text, features="html.parser")
+
     # : h1 ele text
     h1 = html.select('h1[class="dynamictext"]')[0]
-    dictionary['title']= h1.text
-    
+    dictionary['title'] = h1.text
+
     # : short defination
-    Pshort = html.select('div[class="section blurb"]> p[class="short"]')[0] 
+    Pshort = html.select('div[class="section blurb"]> p[class="short"]')[0]
     dictionary['short-summary'] = Pshort.text
 
     # : long defination
@@ -30,11 +36,12 @@ def search(query):
     ## definatio
     ordinal = html.select('h3[class="definition"]')
     for di in ordinal:
-        text = di.text.replace('\n','').replace('\t','').replace('\r','')
+        text = di.text.replace('\n', '').replace('\t', '').replace('\r', '')
         dictionary['definitions'].append(text)
 
     # : return json
     return jsonify(dictionary)
+
 
 if __name__ == "__main__":
     app.run()
--- routes/index.py
+++ routes/index.py
@@ -1,3 +1,2 @@
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(html_doc,'html.parser')
-
+soup = BeautifulSoup(html_doc, 'html.parser')
